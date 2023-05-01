09bc593dcbc001f71586f73cce92efe9a46d1f69
tanuj <[artravellers98@gmail.com]>
2023-05-01 21:14:12 +0530

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
 
+
+const font = Nunito({ subsets: ['latin'] })
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component className={font.className} {...pageProps} />
 }
